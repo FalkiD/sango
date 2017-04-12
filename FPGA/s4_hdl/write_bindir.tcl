10dd8f7a08a3f0ed4f3ff62159179839adc4f43e
Rick Rigby <rmrigby@gmail.com>
2017-04-12 10:30:36 -0400

    Can't get PROJECT directory variable to work yet??

--- FPGA/s4_hdl/write_bindir.tcl
+++ FPGA/s4_hdl/write_bindir.tcl
@@ -1,3 +1,5 @@
+#set PROJECT [get_property DIRECTORY [current_project]]
+# $PROJECT doesn't seem to work, evaluates to .
 file copy -force C:/work/sango/s4-ctrl/hdl/s4_fpga.runs/impl_1/s4.bit C:/work/sango/s4-ctrl/hdl/bin
 file delete -force C:/work/sango/s4-ctrl/hdl/bin/*.prm
 write_cfgmem  -format mcs -size 128 -interface SPIx4 -loadbit "up 0x00000000 C:/work/sango/s4-ctrl/hdl/bin/s4.bit " -force -file "C:/work/sango/s4-ctrl/hdl/bin/s4.mcs"
