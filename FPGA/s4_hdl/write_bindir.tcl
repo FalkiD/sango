aaa6e3531bfab69f207a296181ebe56daa96e74a
Rick Rigby <rmrigby@gmail.com>
2017-04-12 10:10:07 -0400

    Added tcl script to copy bit to bin & generate mcs file

---
+++ FPGA/s4_hdl/write_bindir.tcl
@@ -0,0 +1,3 @@
+file copy -force C:/work/sango/s4-ctrl/hdl/s4_fpga.runs/impl_1/s4.bit C:/work/sango/s4-ctrl/hdl/bin
+file delete -force C:/work/sango/s4-ctrl/hdl/bin/*.prm
+write_cfgmem  -format mcs -size 128 -interface SPIx4 -loadbit "up 0x00000000 C:/work/sango/s4-ctrl/hdl/bin/s4.bit " -force -file "C:/work/sango/s4-ctrl/hdl/bin/s4.mcs"
